86500d2072f69a8ce6549ae78435a7ef530b41d1
Turgay Arif <landofshadow@gmail.com>
2022-03-23 14:46:39 +0200

    Greatest common divisor

---
+++ src/GreatestCommonDivisor.java
@@ -0,0 +1,17 @@
+public class GreatestCommonDivisor {
+
+    public static int getGreatestCommonDivisor(int first, int second) {
+
+        int greatest = 1;
+        if (first >= 10 && second >= 10) {
+            for (int i = 1; ((i <= first) && (i <= second)); i++) {
+                if ((first % i == 0) && (second % i == 0)) {
+                    greatest = i;
+                }
+            }
+            return greatest;
+        } else {
+            return -1;
+        }
+    }
+}
